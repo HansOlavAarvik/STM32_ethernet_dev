4369259f61b9a860e9cd90af050768ad1cdb1127
Hans Olav <hansolavaarvik@gmail.com>
2025-04-08 11:37:52 +0200

    added some fixes

--- Core/Src/json_handle.c
+++ Core/Src/json_handle.c
@@ -34,14 +34,14 @@ static cJSON *outside_temp_array = NULL;
 static cJSON *inside_humidity_array = NULL;
 static cJSON *outside_humidity_array = NULL;
 static cJSON *tof_array = NULL;
-static cJSON *timestamp_array = NULL;
+// static cJSON *timestamp_array = NULL;
 
-//realtime clock handle
-extern RTC_HandleTypeDef hrtc;
+// //realtime clock handle
+// extern RTC_HandleTypeDef hrtc;
 
 // Prototypes
 static VOID json_handle_thread_entry(ULONG thread_input);
-void data_to_json(int16_t data, UINT tag, cJSON* constr);
+void data_to_json(int16_t data, UINT tag);
 void data_to_json_number(int16_t number, const char* string, cJSON* contstru);
 void free_json_string(char* json_string);
 extern UINT UDP_Send(void* data_ptr, UINT data_size, ULONG destination_ip, UINT destination_port);
@@ -49,6 +49,7 @@ UINT add_sensor_data(int16_t data, UINT tag);
 static void transmission_timer_callback(ULONG timer_input);
 void create_arrays(cJSON* root);
 char* get_timestamp(void);
+void data_to_json_string(const char* str, UINT tag) ;
 
 UINT json_handle_initialize(void)
 {
@@ -63,14 +64,14 @@ UINT json_handle_initialize(void)
     return TX_QUEUE_ERROR;
     }
     ret = tx_event_flags_create(&json_events, "JSON Events");
-      if (ret != TX_SUCCESS)
-      {
-          return; //TX_EVENT_FLAGS_ERROR;
-      }
+    if (ret != TX_SUCCESS)
+    {
+        return; // TX_EVENT_FLAGS_ERROR;
+    }
     ret = tx_timer_create(&transmission_timer, "Transmission Timer", 
-      transmission_timer_callback, 0, 
-      300, 300,  
-      TX_AUTO_ACTIVATE);
+                          transmission_timer_callback, 0, 
+                          300, 300,  
+                          TX_AUTO_ACTIVATE);
     if (ret != TX_SUCCESS)
     {
       return TX_TIMER_ERROR;
@@ -83,20 +84,19 @@ UINT json_handle_initialize(void)
     {
       return TX_THREAD_ERROR;
     }
-    RTC_HandleTypeDef hrtc;
-    RTC_TimeTypeDef sTime;
-    RTC_DateTypeDef sDate;
+    // RTC_HandleTypeDef hrtc;
+    // RTC_TimeTypeDef sTime;
+    // RTC_DateTypeDef sDate;
     
-    // Configure RTC (usually in main initialization)
-    hrtc.Instance = RTC;
-    hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
-    hrtc.Init.AsynchPrediv = 127;
-    hrtc.Init.SynchPrediv = 255;
-    HAL_RTC_Init(&hrtc);
+    // // Configure RTC (usually in main initialization)
+    // hrtc.Instance = RTC;
+    // hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
+    // hrtc.Init.AsynchPrediv = 127;
+    // hrtc.Init.SynchPrediv = 255;
+    // HAL_RTC_Init(&hrtc);
 
     return ret;
 }
-
 void json_handle_thread_entry(ULONG thread_input)
 {
     UNUSED(thread_input);
@@ -116,11 +116,11 @@ void json_handle_thread_entry(ULONG thread_input)
         UINT data_count = 0;
 
         while (tx_queue_receive(&sensor_data_queue, &sensor_data, TX_NO_WAIT) == TX_SUCCESS) {
-            data_to_json(sensor_data.value, sensor_data.tag, root);
+            data_to_json(sensor_data.value, sensor_data.tag);
             data_count++;
         }
-        char* timestamp = get_timestamp();
-        data_to_json(timestamp,TIMESTAMP_DATA, root);
+        // char* timestamp = get_timestamp();
+        // data_to_json_string(timestamp,TIMESTAMP_DATA);
         if (data_count > 0) {
             json_data = cJSON_Print(root);
             if (json_data != NULL) {
@@ -140,7 +140,7 @@ UINT add_sensor_data(int16_t data, UINT tag)
     sensor_data.tag = tag;
     return tx_queue_send(&sensor_data_queue, &sensor_data, TX_NO_WAIT);
 }
-void data_to_json(int16_t data, UINT tag, cJSON* constr) {
+void data_to_json(int16_t data, UINT tag){
   switch (tag) {
     case 1: // Vibration
         cJSON_AddItemToArray(vibration_array, cJSON_CreateNumber(data));
@@ -160,11 +160,13 @@ void data_to_json(int16_t data, UINT tag, cJSON* constr) {
     case 6: // Time of flight
         cJSON_AddItemToArray(tof_array, cJSON_CreateNumber(data));
         break;
-    case 7: 
-      cJSON_AddItemToArray(timestamp_array, data);
-      break;
   }
 }
+// void data_to_json_string(const char* str, UINT tag) {
+//   if (tag == 7) { 
+//       cJSON_AddItemToArray(timestamp_array, cJSON_CreateString(str));
+//   }
+// }
 void free_json_string(char* json_string) {
   if (json_string != NULL) {
       cJSON_free(json_string);
@@ -175,7 +177,6 @@ void transmission_timer_callback(ULONG timer_input)
     UNUSED(timer_input);
     tx_event_flags_set(&json_events, JSON_TRANSMIT_EVENT, TX_OR);
 }
-
 void create_arrays(cJSON* root){
   vibration_array = cJSON_CreateArray();
   inside_temp_array = cJSON_CreateArray();
@@ -183,7 +184,7 @@ void create_arrays(cJSON* root){
   inside_humidity_array = cJSON_CreateArray();
   outside_humidity_array = cJSON_CreateArray();
   tof_array = cJSON_CreateArray();
-  timestamp_array = cJSON_CreateArray();
+  //timestamp_array = cJSON_CreateArray();
   
   // Add arrays to root object
   cJSON_AddItemToObject(root, "Vibration", vibration_array);
@@ -192,27 +193,18 @@ void create_arrays(cJSON* root){
   cJSON_AddItemToObject(root, "Inside_humidity", inside_humidity_array);
   cJSON_AddItemToObject(root, "Outside_humidity", outside_humidity_array);
   cJSON_AddItemToObject(root, "Time_of_flight", tof_array);
-  cJSON_AddItemToObject(root, "Timestamp", timestamp_array);
+  //cJSON_AddItemToObject(root, "Timestamp", timestamp_array);
 }
-char* get_timestamp(void)
-{
-  static char timestamp_buffer[20] = "00:00:00"; // Default value
-    
-  // Try with just a static timestamp first to isolate the issue
-  return timestamp_buffer;
-  
-  /* Uncomment this only after the above works
-  ULONG system_ticks = tx_time_get();
-  ULONG seconds = system_ticks / TX_TIMER_TICKS_PER_SECOND;
-  ULONG minutes = seconds / 60;
-  ULONG hours = minutes / 60;
-  
-  seconds %= 60;
-  minutes %= 60;
-  hours %= 24;
-  
-  sprintf(timestamp_buffer, "%02lu:%02lu:%02lu", hours, minutes, seconds);
-  
-  return timestamp_buffer;
-  */
-}
+// char* get_timestamp(void)
+// {
+//   static char timestamp_buffer[20];
+//   ULONG system_ticks = tx_time_get();
+//   ULONG seconds = system_ticks / TX_TIMER_TICKS_PER_SECOND;
+//   ULONG minutes = seconds / 60;
+//   ULONG hours = minutes / 60; 
+//   seconds %= 60;
+//   minutes %= 60;
+//   hours %= 24;
+//   sprintf(timestamp_buffer, "%02lu:%02lu:%02lu", hours, minutes, seconds);
+//   return timestamp_buffer;
+// }
